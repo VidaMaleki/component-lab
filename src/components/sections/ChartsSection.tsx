22ab653c4c86076f55f56edbe64b8dc168ee4b2e
VidaMaleki <vida.basmani@gmail.com>
2026-01-18 22:28:05 -0800

    fixed build error for entry

--- src/components/sections/ChartsSection.tsx
+++ src/components/sections/ChartsSection.tsx
@@ -255,7 +255,7 @@ function AreaChartComponent() {
                                     fill="#8884d8"
                                     dataKey="value"
                                 >
-                                    {pieData.map((entry, index) => (
+                                    {pieData.map((_, index) => (
                                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                                     ))}
                                 </Pie>
